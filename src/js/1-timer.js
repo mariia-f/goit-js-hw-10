c7c414ad2acee562298929ed3a5daeaf4532645c
mariia <maryfurman101107@gmail.com>
2026-02-17 22:12:24 +0200

    fixed

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,13 +1,8 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-
-
 let userSelectedDate = "";
+let timerId;
+
 const btn = document.querySelector(".timer-btn");
-const inp = document.querySelector(".input") 
-inp.disabled = false;
+const inp = document.querySelector("#datetime-picker");
 
 const options = {
   enableTime: true,
@@ -15,51 +10,23 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-      console.log(selectedDates[0]);
-      userSelectedDate = selectedDates[0]
-      if (userSelectedDate < new Date()) {
-       iziToast.error({
+    userSelectedDate = selectedDates[0];
+
+    if (userSelectedDate <= new Date()) {
+      iziToast.error({
         title: 'Error',
         message: 'Please choose a date in the future',
         position: 'topRight',
       });
-    
-    btn.disabled = true;
-   
-}
-else {
-    btn.disabled = false;
-    inp.disabled = false; 
-}
+
+      btn.disabled = true;
+    } else {
+      btn.disabled = false;
+    }
   },
 };
 
-flatpickr("#datetime-picker", options)
-
-const daysEl = document.querySelector('[data-days]');
-const hoursEl = document.querySelector('[data-hours]');
-const minutesEl = document.querySelector('[data-minutes]');
-const secondsEl = document.querySelector('[data-seconds]');
-
-function convertMs(ms) {
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  const days = Math.floor(ms / day);
-  const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
-
-
-function addLeadingZero(value) {
-  return String(value).padStart(2, '0');
-}
-
+flatpickr(inp, options);
 
 btn.addEventListener('click', () => {
   if (!userSelectedDate) return;
@@ -79,13 +46,13 @@ btn.addEventListener('click', () => {
       minutesEl.textContent = "00";
       secondsEl.textContent = "00";
 
-      inp.disabled = false; // після завершення таймера можна обирати нову дату
+      inp.disabled = false;
       return;
     }
 
     const { days, hours, minutes, seconds } = convertMs(ms);
 
-    daysEl.textContent = days;
+    daysEl.textContent = addLeadingZero(days);
     hoursEl.textContent = addLeadingZero(hours);
     minutesEl.textContent = addLeadingZero(minutes);
     secondsEl.textContent = addLeadingZero(seconds);
